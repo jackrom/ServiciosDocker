a22b3d01a7a188f3195b95017b3530d4c1686fae
juan.reyes <juan.reyes@coremain.com>
2024-04-19 17:22:34 -0500

    Initial commit

---
+++ comandos.sh
@@ -0,0 +1,7 @@
+# Swarm initialized: current node (9n5jv45vhcu6ywe0b630lzfgy) is now a manager.
+
+# To add a worker to this swarm, run the following command:
+
+#    docker swarm join --token SWMTKN-1-1dfz7vc68tmhzhdq255suqr8hg7kaqn6zx257a23outsomw3h6-9jsnqxil33ts51zholb45e2lz 10.128.0.2:2377
+
+# To add a manager to this swarm, run 'docker swarm join-token manager' and follow the instructions.
